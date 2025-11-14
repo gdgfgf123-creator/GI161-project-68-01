96b22ced69f43e50f937b201c8c644c33a738f80
Itt <gdgfgf123@gmail.com>
2025-11-14 21:05:02 +0700

    Make scripts to warp to other maps by interacting with objects.

--- Assets/Scripts/TP.cs
+++ Assets/Scripts/TP.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TP : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string sceneName;
+    public TMP_Text interactText;
+    public KeyCode key = KeyCode.F;
+
+    private bool canSwitch = false;
+
     void Start()
     {
-        
+        interactText.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (canSwitch && Input.GetKeyDown(key))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    // สำคัญ! 2D ต้องใช้ OnTriggerEnter2D
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canSwitch = true;
+            interactText.text = "Press F to respond";
+            interactText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canSwitch = false;
+            interactText.gameObject.SetActive(false);
+        }
     }
 }
